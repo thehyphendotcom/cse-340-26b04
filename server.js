260710cc0365e1b5efd5397bd50088b5a40af893
The Hyphen <thehyphen.com@gmail.com>
2026-07-13 20:28:07 -0600

    Projects info displayed

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import { testConnection } from './src/models/db.js';
 import { getAllOrganizations } from './src/models/organizations.js';
+import { getAllProjects } from './src/models/projects.js';
 
 // Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -44,8 +45,12 @@ app.get('/organizations', async (req, res) => {
 });
 
 app.get('/projects', async (req, res) => {
+    const projects = await getAllProjects();
+    console.log('Projects:', projects); // Log the projects to the console for debugging
+
     const title = 'Service Projects';
-    res.render('projects', { title });
+    
+    res.render('projects', { title, projects });
 });
 
 app.get('/categories', async (req, res) => {
