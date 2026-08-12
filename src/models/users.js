5629458f6d1b4c924df0ad75b9272347e2228eab
The Hyphen <thehyphen.com@gmail.com>
2026-08-12 15:35:22 -0600

    Fixed issue: name not rendered in dashboard.

--- src/models/users.js
+++ src/models/users.js
@@ -25,7 +25,7 @@ const createUser = async (name, email, passwordHash) => {
 
 const findUserByEmail = async (email) => {
     const query = `
-    SELECT u.user_id, u.email, u.password_hash, r.role_name 
+    SELECT u.user_id, u.name, u.email, u.password_hash, r.role_name 
     FROM users u
     JOIN roles r ON u.role_id = r.role_id
     WHERE u.email = $1
